941838b5e9bed5a15dd53f1609a0f6624f05fc03
Seyderhelm <f.seyderhelm@stud.uni-hannover.de>
2022-08-03 20:46:45 +0200

    testCommit

---
+++ api/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+#[default]
+#aws_access_key_id = "YOUR_ACCESS_KEY"
+#aws_secret_access_key = "YOUR_SECRET_KEY"
+
+#[staging]
+#aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
+#aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+#[prod]
+aws_access_key_id = "AKIAIOSFODNN7KJSNJKS"
+aws_secret_access_key = "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"
